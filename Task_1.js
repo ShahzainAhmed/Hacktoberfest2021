0fbb03165225a8e49e733faef1397dd03c84f1a3
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2021-10-05 16:04:19 +0500

    Hacktoberfest
    added code

--- Task_1.js
+++ Task_1.js
@@ -1,17 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function(){ // This is the ready function
 
-  $('.input').focus(function(){
+  $('.input').focus(function(){ // This is the focus function
     $(this).parent().find(".label-txt").addClass('label-active');
   });
 
-  $(".input").focusout(function(){
+  $(".input").focusout(function(){ // taking the input
     if ($(this).val() == '') {
       $(this).parent().find(".label-txt").removeClass('label-active');
       console.log("This is JavaScript, pulled by Shahzain Ahmed");
-      console.log("Work in progress!");
+      console.log("Work in progress!"); // print statements
       console.log("New pull requests being held at Web Engineering Labs");
-      console.log("18 batch, Computer Systems Department (Section II)");
-      console.log("Computer Systems Engineering");
     };
   });
 
