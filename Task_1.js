b66a361775556c18ad8b04a07e22a76d5e7f0754
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2021-10-05 15:26:24 +0500

    Hactoberfest
    Updated the console.log statements and fixed the wrong ones with the new correct ones.

--- Task_1.js
+++ Task_1.js
@@ -7,8 +7,9 @@ $(document).ready(function(){
   $(".input").focusout(function(){
     if ($(this).val() == '') {
       $(this).parent().find(".label-txt").removeClass('label-active');
-      console.log("KEHRA HAL THAI");
+      console.log("This is JavaScript, pulled by Shahzain Ahmed");
       console.log("Work in progress!");
+      console.log("New pull requests being held at Web Engineering Labs");
     };
   });
 
