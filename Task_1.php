00bef766ad0861b2aab408f36ab4e75b70604e83
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2021-10-05 15:37:48 +0500

    Added comments for better user readability.
    Comments are necessary for better user readability.

--- Task_1.php
+++ Task_1.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>Add Records in Database</title>
+    <title>Add Records in Database</title> // This goes for the title
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="style.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-	<form action="" method="post">
+	<form action="" method="post"> // Using the POST method here, instead of GET method.
         <label>
             <input type="text" class="input" name="name" placeholder="ENTER YOUR FULl NAME">
             <div class="line-box">
@@ -49,10 +49,12 @@
 			if(!$insert){
 				echo mysqli_error();
 			}else{
-				echo "Records added successfully.";
+				echo "Records have been added successfully."; // printing the records being successfully addeed.
+				echo "Successfully done.";
+				
 			}
         }
-	   mysqli_close($db); // Close connection
+	   mysqli_close($db); // Closing the connection
     ?>
 
     <script src="Task_1.js"></script>
